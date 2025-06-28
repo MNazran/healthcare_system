19ca1166a2d2a0430a89202b113f3292b8b02dfb
MNazran <muhammadnazran78@gmail.com>
2025-06-28 12:10:23 +0800

    Enhance PatientFormSchema: add fields for date of birth, gender, phone, etc; Implement patient registration form with validation schema and user data integration

--- components/new-patient.tsx
+++ components/new-patient.tsx
@@ -3,6 +3,13 @@
 import { Patient } from '@/lib/generated/prisma';
 import { useUser } from '@clerk/nextjs';
 import React, { useState } from 'react';
+import { Card, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { useRouter } from 'next/navigation';
+import { Form } from './ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { PatientFormSchema } from '@/lib/schema';
+import z from 'zod';
 
 interface DataProps {
   data?: Patient;
@@ -11,6 +18,9 @@ interface DataProps {
 
 export const NewPatient = ({ data, type }: DataProps) => {
   const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+  const [imgURL, setImgURL] = useState<any>();
+  const router = useRouter();
 
   const userData = {
     first_name: user?.firstName || '',
@@ -19,5 +29,25 @@ export const NewPatient = ({ data, type }: DataProps) => {
     phone: user?.phoneNumbers?.toString() || '',
   };
 
-  return <div>NewPatient</div>;
+  type PatientFormType = z.infer<typeof PatientFormSchema>;
+  const form = useForm<PatientFormType>({
+    resolver: zodResolver(PatientFormSchema) as any,
+    defaultValues: {
+      ...userData,
+    },
+  });
+
+  return (
+    <Card className='max-w-6xl w-full p-4'>
+      <CardHeader>
+        <CardTitle>Patient Registration</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <Form {...form}>
+        <form onSubmit={() => {}} className='space-y-8 mt-5'>
+          <h3 className='text-lg font-semibold'>Personal Information</h3>
+        </form>
+      </Form>
+    </Card>
+  );
 };
