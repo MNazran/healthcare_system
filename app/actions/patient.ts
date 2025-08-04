090b884bbaea3af3ebb6fea1ee9e85348ce3632e
MNazran <muhammadnazran78@gmail.com>
2025-08-04 21:30:28 +0800

    Implement patient creation logic and add database integration

--- app/actions/patient.ts
+++ app/actions/patient.ts
@@ -2,6 +2,7 @@
 
 import { PatientFormSchema } from '@/lib/schema';
 import { clerkClient } from '@clerk/nextjs/server';
+import db from '@/lib/db';
 
 export async function createNewPatient(data: any, pid: string) {
   try {
@@ -18,9 +19,9 @@ export async function createNewPatient(data: any, pid: string) {
     const patientData = validateData.data;
     let patient_id = pid;
 
-    if (pid === 'new-patient') {
-      const client = await clerkClient();
+    const client = await clerkClient();
 
+    if (pid === 'new-patient') {
       const user = await client.users.createUser({
         emailAddress: [patientData.email],
         password: patientData.phone,
@@ -28,7 +29,22 @@ export async function createNewPatient(data: any, pid: string) {
         lastName: patientData.last_name,
         publicMetadata: { role: 'patient' },
       });
+
+      patient_id = user?.id;
+    } else {
+      await client.users.updateUser(pid, {
+        publicMetadata: { role: 'patient' },
+      });
     }
+
+    await db.patient.create({
+      data: {
+        ...patientData,
+        id: patient_id,
+      },
+    });
+
+    return { success: true, error: false, msg: 'Patient create successfully' };
   } catch (error: any) {
     console.error(error);
     return { success: false, error: true, msg: error?.message };
